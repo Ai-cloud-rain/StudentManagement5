e6eda76ec6c91c725483eb6e3d04a059b894a305
Ai-cloud-rain <91471536+Ai-cloud-rain@users.noreply.github.com>
2024-12-01 10:34:59 +0900

    Add basic controller setup

--- src/main/java/raisetech/StudentManagement5/Application.java
+++ src/main/java/raisetech/StudentManagement5/Application.java
@@ -1,10 +1,16 @@
 package raisetech.StudentManagement5;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+  @Autowired
+  private StudentRepository repository;
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
