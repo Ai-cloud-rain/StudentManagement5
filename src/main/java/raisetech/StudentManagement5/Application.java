f15bad899195dc00c2b4412fe735d5da66eb3ef4
Ai-cloud-rain <91471536+Ai-cloud-rain@users.noreply.github.com>
2024-12-01 10:58:09 +0900

    Implement functionality to retrieve all records from the student table

--- src/main/java/raisetech/StudentManagement5/Application.java
+++ src/main/java/raisetech/StudentManagement5/Application.java
@@ -1,8 +1,10 @@
 package raisetech.StudentManagement5;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,6 +16,11 @@ public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @GetMapping("/studentList")
+  public List<Student> getStudentList() {
+    return repository.searchStudent();
   }
 
 }
--- src/main/java/raisetech/StudentManagement5/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement5/StudentRepository.java
@@ -1,8 +1,13 @@
 package raisetech.StudentManagement5;
 
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface StudentRepository {
+
+  @Select("SELECT * FROM student")
+  List<Student> searchStudent();
 
 }
